cc9f652d3282f95774a2ced75313f69db2ab5653
Axel Hoffmann <axel.hoffmann-ext@continental-corporation.com>
2015-05-24 22:23:39 +0200

    Added calibre-convert bash script and convert makefile

---
+++ convert_bash_script/calibre-convert.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+IFS=$'\n'
+formats=('epub' 'mobi' 'pdf')
+
+exist_in_array() {
+    local element=$1 && shift
+    local array=($@)
+
+    for item in "${array[@]}"
+    do
+        if [[ $item == $element ]]
+            then return 0
+        fi
+    done
+    return 1
+}
+
+
+#find_formats=`echo ${formats[@]} | sed 's/\</\*\./g;s/\>/\n/g;s/\ //g'`
+find_formats=`echo ${formats[@]} | sed 's/\>/"/g;s/\</-iname\ "\*\./g;s/-iname/-or\ -iname/2g'`
+
+filenames=($(eval "find . -type f $find_formats" | sort))
+
+books=()
+for file in "${filenames[@]}"
+do
+    if ! exist_in_array ${file%.*} ${books[@]}
+        then books+=(${file%.*})
+    fi
+done
+
+#books=($(echo -e ${filenames[@]} | sed 's/\<\(.*\)\..*\>/\1/g' | sort | uniq))
+#books=($(eval "find . -type f $find_formats" | sed 's/\(.*\)\..*/\1/' | sort | uniq))
+
+echo ${filenames[@]}
+echo ${books[@]}
+
+#books=($(find . -type f | sed 's/\(.*\)\..*/\1/' | sort | uniq ))
+#b=('blubbb' 'blaaa' 'wakken sd')
+#
+#if exist_in_array "wakken sd" ${b[@]} 
+#    then echo "ja"
+#else
+#    echo "nein"
+#fi
+
+for book in books
+do
+    for tformat in formats
+    do
+        if ! exist_in_array $book.$tformat ${filenames[@]}
+            then for sformat in 
+        fi
+    done
+done
+
+
+
